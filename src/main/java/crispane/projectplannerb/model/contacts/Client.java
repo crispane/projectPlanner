3810d0afe6212f56ee939c3cb268379105c78dd0
crispane <crispane@pm.me>
2022-07-30 17:01:02 +0300

    Insert values using CommandLineRunner

--- src/main/java/crispane/projectplannerb/model/contacts/Client.java
+++ src/main/java/crispane/projectplannerb/model/contacts/Client.java
@@ -1,7 +1,6 @@
 package crispane.projectplannerb.model.contacts;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,11 +10,14 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Client {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     private String name;
 }
