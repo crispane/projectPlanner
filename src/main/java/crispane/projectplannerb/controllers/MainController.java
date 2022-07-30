292c67a4decc8423e5a907939fef59b08bd3f6f2
crispane <crispane@pm.me>
2022-07-30 20:17:38 +0300

    Insert values by thymeleaf

--- src/main/java/crispane/projectplannerb/controllers/MainController.java
+++ src/main/java/crispane/projectplannerb/controllers/MainController.java
@@ -2,19 +2,28 @@ package crispane.projectplannerb.controllers;
 
 import crispane.projectplannerb.data.ClientRepository;
 import crispane.projectplannerb.model.contacts.Client;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @Controller
-@RequestMapping("/demo")
+@RequestMapping("/client")
 public class MainController {
 
-    @Autowired
-    private ClientRepository clientRepository;
+    private final ClientRepository clientRepository;
+
+    @ModelAttribute(name = "client")
+    public Client client(){
+        return new Client();
+    }
+
+    public MainController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
     @PostMapping("/add")
-    public @ResponseBody String addNewClient (@RequestParam String name) {
+    public @ResponseBody String addNewClient(@RequestParam String name) {
 
         Client client = new Client();
         client.setName(name);
@@ -22,9 +31,19 @@ public class MainController {
         return "Saved";
     }
 
+    @GetMapping
+    public String showClientForm() {
+        return "client";
+    }
+
     @GetMapping("/all")
     public @ResponseBody Iterable<Client> getAllClients() {
         return clientRepository.findAll();
     }
-}
 
+    @PostMapping
+    public String processClient(@Valid Client client) {
+        clientRepository.save(client);
+        return "client";
+    }
+}
--- src/main/java/crispane/projectplannerb/model/contacts/Client.java
+++ src/main/java/crispane/projectplannerb/model/contacts/Client.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -19,5 +20,6 @@ public class Client {
     private Long id;
 
     @NonNull
+    @Size(min=3, message="Name must be at least 3 characters long.")
     private String name;
 }
